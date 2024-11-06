055e44954d466fcbcb4aea1efdf3ff192d6c6bf3
fahim <fahim1998108@gmail.com>
2024-11-06 23:09:48 +0530

    commit

---
+++ src/main/java/lk/ijse/possystembackendspring/dao/OrderDao.java
@@ -0,0 +1,9 @@
+package lk.ijse.possystembackendspring.dao;
+
+import lk.ijse.possystembackendspring.Entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDao  extends JpaRepository<Orders,String> {
+}
